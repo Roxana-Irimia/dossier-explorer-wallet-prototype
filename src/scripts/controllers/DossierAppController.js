30932fd58249836d4c1f4b29a6a7885556ef37f0
Mastaleru Rafael <mastaleru.rafael@gmail.com>
2020-03-18 12:54:30 +0200

    added DossierAppController

---
+++ src/scripts/controllers/DossierAppController.js
@@ -0,0 +1,35 @@
+import ApplicationController from "./ApplicationController.js";
+
+const lazyItemsModel = [{
+    name: "App1",
+    path: "/my-apps/path1",
+    exact: true,
+    icon: "fa-home",
+    component: "psk-page-loader",
+    componentProps: {pageUrl: "http://localhost:8000/pages/nested-menu/item1.html"}
+},
+    {
+        name: "App2",
+        path: "/my-apps/path2",
+        icon: "fa-home",
+        exact: true,
+        componentProps: {pageUrl: "http://localhost:8000/pages/nested-menu/item1.html"}
+    }];
+
+export default class DossierAppController extends ApplicationController {
+
+    constructor(element) {
+        super(element);
+
+        element.addEventListener("getSSApps", function (event) {
+            if (typeof event.getEventType === "function" && event.getEventType() === "PSK_SUB_MENU_EVT") {
+                let callback = event.data;
+                if (typeof callback !== "function") {
+                    throw new Error("Callback should be a function")
+                }
+                setTimeout(()=>{callback(undefined, lazyItemsModel);},500);
+            }
+        })
+    }
+
+}
