f2d9f05a0ee3126f99e1c6ddd11a453210279da1
Mastaleru Rafael <mastaleru.rafael@gmail.com>
2020-03-31 00:16:59 +0300

    disabled DossierExplorerService.js and service worker

--- src/scripts/service/DossierExplorerService.js
+++ src/scripts/service/DossierExplorerService.js
@@ -12,15 +12,15 @@ class DossierExplorerService {
             path = "/";
         }
 
-        setTimeout(
-            ()=>{
-                const HostBootScript = require("boot-host").HostBootScript;
-                const HostPC = new HostBootScript("dossier-explorer");
-                console.log("Starting listDosierFiles interactions");
-                $$.interactions.startSwarmAs("test/agent/007", "listDossierFiles", "start", path)
-                    .onReturn(callback);
-            },10000
-        );
+        // setTimeout(
+        //     ()=>{
+        //         const HostBootScript = require("boot-host").HostBootScript;
+        //         const HostPC = new HostBootScript("dossier-explorer");
+        //         console.log("Starting listDosierFiles interactions");
+        //         $$.interactions.startSwarmAs("test/agent/007", "listDossierFiles", "start", path)
+        //             .onReturn(callback);
+        //     },10000
+        // );
 
     }
 
