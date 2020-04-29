9a114982e6b5658c51d8ca42878a289c32888e01
paiusCatalin <p.cata95@gmail.com>
2020-04-29 15:10:53 +0300

    share dossier refactor

--- code/scripts/view-models/shareDossierModal.js
+++ code/scripts/view-models/shareDossierModal.js
@@ -1,38 +1,42 @@
 const shareDossierModal = {
   title: "Share",
-  disclaimer:
-    "To share, we need to scan a QR code or enter a Collaborator Secure Code to identify to whom you want to send.",
-  loadingDisclaimer:
-    "We are waiting for your collaborator to receive the dossier",
-  confirmedShareDisclaimer:
-    "Your collaborator has received the dossier succesfuly!",
+  disclaimer: "To share, we need to scan a QR code or enter a Collaborator Secure Code to identify to whom you want to send.",
+  loadingDisclaimer: "We are waiting for your collaborator to receive the dossier",
+  confirmedShareDisclaimer: "Your collaborator has received the dossier succesfuly!",
   orLabel: "or",
   useQrCodeLabel: "Use QR Code",
-  getIdentityState: 1,
-  sendLoading: 0,
-  hasError: 0,
-  errorMessage: "",
-  enterCode: {
+  identitySecureCodeInput: {
     value: "",
     placeholder: "e.g. %rt78qtp%$",
     label: "Enter Secure Code",
   },
-  scanBtn: {
-    label: "Scan",
-    eventName: "scan-identity",
-    buttonClass: "btn-confirm-secondary",
+  buttons: {
+    scanIdentityButton: {
+      label: "Scan",
+      eventName: "scan-identity",
+      buttonClass: "btn-confirm-secondary",
+    },
+    sendDossierButton: {
+      label: "Send",
+      eventName: "start-share",
+      buttonClass: "btn-confirm-primary",
+    },
+    finishButton: {
+      disabled: false,
+      label: "Finish",
+      eventName: "finish-share",
+      buttonClass: "btn-confirm-primary",
+    },
   },
-  sendBtn: {
-    label: "Send",
-    eventName: "start-share",
-    buttonClass: "btn-confirm-primary",
+  error: {
+    hasError: 0,
+    errorMessage: ""
   },
-  finishBtn: {
-    disabled: false,
-    label: "Finish",
-    eventName: "finish-share",
-    buttonClass: "btn-confirm-primary",
-  },
+  conditionalExpressions: {
+    isIdentitySelectionStep: true,
+    isSendDossierStep: false,
+    isLoading: false,
+  }
 };
 
 export default shareDossierModal;
