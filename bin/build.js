24421ca1830fff1d017f7f3511d3a83ea6a4123c
Vlad Balmos <vlad.balmos@code932.com>
2020-05-11 16:35:27 +0300

    Bug fix

--- bin/build.js
+++ bin/build.js
@@ -49,8 +49,13 @@ function build(callback) {
         if (err || content.length === 0) {
             return createWallet(callback);
         }
-        const bar = edfs.loadBar(content);
-        updateWallet(bar, callback);
+        edfs.loadBar(content, (err, bar) => {
+            if (err) {
+                return callback(err);
+            }
+
+            updateWallet(bar, callback);
+        });
     });
 }
 
