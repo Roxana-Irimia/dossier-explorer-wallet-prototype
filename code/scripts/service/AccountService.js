3e736c0b6d9049650c67695af4d88f8f707a8b19
Mastaleru Rafael <mastaleru.rafael@gmail.com>
2020-05-29 17:50:11 +0300

    completed sign-out implementation

--- code/scripts/service/AccountService.js
+++ code/scripts/service/AccountService.js
@@ -1,21 +1,10 @@
+import LoaderCommunication from "./LoaderCommunication.js";
 class AccountService {
 
-	/**
-	 * implement a mechanism to talk with loader and communicate that it have to unregister the service worker
-	 * @param preferences
-	 */
 	signOut(preferences) {
-		if (preferences.deleteSeed) {
-			this.forgetWallet();
-		}
-		window.location.reload();
-	}
-
-	forgetWallet() {
-		localStorage.clear();
+		LoaderCommunication.sendMessage({status:"sign-out", deleteSeed:preferences.deleteSeed});
 	}
 }
-
 
 let accountService = new AccountService();
 let getAccountServiceInstance = function () {
