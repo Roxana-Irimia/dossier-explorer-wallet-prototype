2cf719eaaf038920689d10224b10c20d23306a98
Nicu-Cosmin Ursache <asac.cool@gmail.com>
2020-06-02 19:08:02 +0300

    build script update

--- bin/build.js
+++ bin/build.js
@@ -1,6 +1,5 @@
 const DOSSIER_SEED_FILE_PATH = "./seed";
-const BRICK_STORAGE_PORT = process.env.BRICK_STORAGE_PORT || 8080;
-const BRICK_STORAGE_ENDPOINT = `http://127.0.0.1:${BRICK_STORAGE_PORT}`;
+const BRICK_STORAGE_ENDPOINT = process.env.SSAPPS_FAVORITE_EDFS_ENDPOINT || "http://127.0.0.1:8080";
 
 require("./../../privatesky/psknode/bundles/csbBoot.js");
 require("./../../privatesky/psknode/bundles/edfsBar.js");
@@ -26,8 +25,6 @@ function createWallet(callback) {
 }
 
 function updateWallet(bar, callback) {
-
-    console.log("update wallet");
     bar.delete("/", function(err){
         if(err){
             throw err;
@@ -46,8 +43,25 @@ function updateWallet(bar, callback) {
 function build(callback) {
     fs.readFile(DOSSIER_SEED_FILE_PATH, (err, content) => {
         if (err || content.length === 0) {
+            console.log(`Creating a new Dossier...`);
             return createWallet(callback);
         }
+
+        const SEED = require("bar").Seed;
+        let seed;
+        try {
+            seed = new SEED(content);
+        } catch (err) {
+            console.log("Invalid seed. Creating a new Dossier...");
+            return createWallet(callback);
+        }
+
+        if(seed.getEndpoint() !== BRICK_STORAGE_ENDPOINT){
+            console.log("Endpoint change detected. Creating a new Dossier...");
+            return createWallet(callback);
+        }
+
+        console.log("Dossier updating...");
         edfs.loadBar(content, (err, bar) => {
             if (err) {
                 return callback(err);
@@ -59,10 +73,12 @@ function build(callback) {
 }
 
 build(function (err, seed) {
+    let path = require("path");
+    let projectName = path.basename(path.join(__dirname, "../"));
     if (err) {
+        console.log(`Build process of <${projectName}> failed.`);
         console.log(err);
         process.exit(1);
     }
-
-    console.log("DossierExplorer-Wallet Seed:", seed);
+    console.log(`Build process of <${projectName}> finished. Dossier Seed:`, seed);
 });
