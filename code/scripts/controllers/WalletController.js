db4202e57cd524cd9c672033a4d14f094c9dd2d8
paiusCatalin <p.cata95@gmail.com>
2020-11-18 15:57:01 +0200

    displayed installed applications on left menu

--- code/scripts/controllers/WalletController.js
+++ code/scripts/controllers/WalletController.js
@@ -1,106 +1,80 @@
 import ContainerController from "../../cardinal/controllers/base-controllers/ContainerController.js";
 
-import { getDossierServiceInstance } from "../service/DossierExplorerService.js"
-import { getAccountServiceInstance } from "../service/AccountService.js";
+import {getDossierServiceInstance} from "../service/DossierExplorerService.js"
+import {getAccountServiceInstance} from "../service/AccountService.js";
 
 import signOutViewModel from "../view-models/modals/signOutViewModel.js";
 
-const DossierExplorerService = getDossierServiceInstance();
-const APPS_FOLDER = "/apps/psk-marketplace-ssapp/my-apps";
-const MARKETPLACES_FOLDER = "/marketplaces";
-
-const appTemplate = {
-	exact: false,
-	component: "psk-ssapp",
-	componentProps: {}
-};
-
 export default class WalletController extends ContainerController {
-	constructor(element, history) {
-		super(element, history);
-		element.addEventListener("sign-out", this._signOutFromWalletHandler);
-		element.addEventListener("getSSApps", this._getSSAppsHandler);
-	}
+    constructor(element, history) {
+        super(element, history);
 
-	_getSSAppsHandler = (event) => {
-		if (typeof event.getEventType === "function" &&
-			event.getEventType() === "PSK_SUB_MENU_EVT") {
+        this.APPS_FOLDER = "/apps";
+        this.DossierExplorerService = getDossierServiceInstance();
 
-			let callback = event.data.callback;
-			let pathPrefix = event.data.pathPrefix;
-			if (typeof callback !== "function") {
-				throw new Error("Callback should be a function");
-			}
+        element.addEventListener("sign-out", this._signOutFromWalletHandler);
+        element.addEventListener("getSSApps", this._getSSAppsHandler);
+    }
 
-			DossierExplorerService.readDirDetailed(MARKETPLACES_FOLDER, (err, data) => {
-				if (err) {
-					return callback(err);
-				}
-				let mounts = data.applications;
+    _getSSAppsHandler = (event) => {
+        if (typeof event.getEventType === "function" &&
+            event.getEventType() === "PSK_SUB_MENU_EVT") {
 
-				let auxApps = [];
+            let callback = event.data.callback;
+            let pathPrefix = event.data.pathPrefix;
+            if (typeof callback !== "function") {
+                throw new Error("Callback should be a function");
+            }
 
-				let chain = (applications) => {
-					if (applications.length === 0) {
-						return callback(err, auxApps);
-					}
-					let mountedApp = applications.shift();
-					let marketplacePath = MARKETPLACES_FOLDER + '/' + mountedApp + '/my-apps';
-					this.getAppsFromMarketplace(marketplacePath, pathPrefix, (err, apps) => {
-						if (err) {
-							return callback(err);
-						}
-						auxApps = [...auxApps, ...apps];
+            this.DossierExplorerService.readDirDetailed(this.APPS_FOLDER, (err, {applications}) => {
+                if (err) {
+                    return callback(err);
+                }
 
-						chain(mounts);
-					});
-				}
-				chain(mounts);
-			});
-		}
-	}
+                let mountedApplications = [];
+                let chain = (appNamesList) => {
+                    if (!appNamesList.length) {
+                        return callback(err, mountedApplications);
+                    }
 
-	getAppsFromMarketplace(marketplacePath, pathPrefix, callback) {
-		DossierExplorerService.readDirDetailed(marketplacePath, (err, {mounts}) => {
-			if (err) {
-				return callback(err);
-			}
-			let auxApps = [];
+                    let appName = appNamesList.pop();
+                    this.DossierExplorerService.printDossierSeed(this.APPS_FOLDER, appName, (err, appSSI) => {
+                        if (err) {
+                            console.error(err);
+                            return chain(appNamesList);
+                        }
 
-			let appChain = (mounts) => {
-				if (mounts.length === 0) {
-					return callback(err, auxApps);
-				}
-				let mountedApp = mounts.shift();
-				let path = marketplacePath + '/' + mountedApp;
+                        let app = this._getApplicationTemplate();
+                        app.name = appName;
+                        app.path = pathPrefix + "/" + appSSI;
+                        app.componentProps.appName = appName;
+                        app.componentProps.keySSI = appSSI;
+                        mountedApplications.push(app);
+                        chain(appNamesList);
+                    });
+                };
 
-				let app = JSON.parse(JSON.stringify(appTemplate));
-				app.path = pathPrefix + '/' + mountedApp;
+                chain(applications);
+            });
+        }
+    };
 
-				this.DSUStorage.getObject(path + '/data', (err, data) => {
-					if (err) {
-						console.log(err);
-						return;
-					}
-					app.name = data.name;
-					app.componentProps.appName = mountedApp;
-					app.componentProps.keySSI = data.keySSI;
-					auxApps.push({...app});
-					appChain(mounts);
-				});
-			}
-			appChain(mounts);
-		});
-	}
+    _signOutFromWalletHandler = (event) => {
+        event.preventDefault();
+        event.stopImmediatePropagation();
 
-	_signOutFromWalletHandler = (event) => {
-		event.preventDefault();
-		event.stopImmediatePropagation();
+        this.showModal("signOutModal", signOutViewModel, (err, preferences) => {
+            if (!err) {
+                getAccountServiceInstance().signOut(preferences);
+            }
+        });
+    };
 
-		this.showModal("signOutModal", signOutViewModel, (err, preferences) => {
-			if (!err) {
-				getAccountServiceInstance().signOut(preferences);
-			}
-		});
-	};
+    _getApplicationTemplate() {
+        return {
+            exact: false,
+            component: "psk-ssapp",
+            componentProps: {}
+        };
+    }
 }
