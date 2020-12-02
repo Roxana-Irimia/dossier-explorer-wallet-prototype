58793dee2804736b6ec07ef8365ee15de12f3bed
paiusCatalin <p.cata95@gmail.com>
2020-12-02 11:32:19 +0200

    cleanup

--- bin/build.js
+++ bin/build.js
@@ -1,13 +1,11 @@
 const DOSSIER_SEED_FILE_PATH = "./seed";
 const CARDINAL_SEED_FILE_PATH = "../cardinal/seed";
 const THEMES_PATH = "../themes";
-const BRICK_STORAGE_ENDPOINT = process.env.SSAPPS_FAVORITE_EDFS_ENDPOINT || "http://localhost:8080";
 const DEFAULT_DOMAIN = "default";
 
 require("./../../privatesky/psknode/bundles/openDSU.js");
 const fs = require("fs");
 const openDSU = require("opendsu");
-const bdns = openDSU.loadApi("bdns");
 const keyssi = openDSU.loadApi("keyssi");
 const resolver = openDSU.loadApi("resolver");
 
